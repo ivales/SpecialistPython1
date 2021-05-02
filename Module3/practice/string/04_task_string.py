73daa16dd220a3d9985f60b9843e5b15f34cffcf
ivales <83123684+ivales@users.noreply.github.com>
2021-05-02 14:41:12 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,10 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+words = text.split(' ')
+counter=i=0
+while i<len(words):
+    if len(words[i])>5:
+        counter+=1
+    i+=1
+print(counter)
