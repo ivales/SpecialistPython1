7bbc4173db4fa1a884abbd18e08237e8ea3b2a0f
ivales <83123684+ivales@users.noreply.github.com>
2021-05-02 17:06:19 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,11 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+tup1=4,6,8,10
+tup2=4,6
+tup3=4,6,8,11,12,1
+counter=0
+for e in tup1:
+    if tup2.count(e)!=0 and tup3.count(e)!=0:
+        counter+=max(tup2.count(e), tup3.count(e))
+print(counter)
