414fd6a48a18c0b85d05dd4d71444c4b3c43f38d
ivales <83123684+ivales@users.noreply.github.com>
2021-05-02 17:39:06 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,9 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+source_array=[2, -5, 8, 9, -25, 25, 4]
+final_array=[]
+for el in source_array:
+    if el>0 and (int(el)**(1/2)).is_integer():
+        final_array.append(int(el**(1/2)))
+print(final_array)
