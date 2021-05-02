bf54e7786b2fcfb71ecbe2791a103e458345e2e0
ivales <83123684+ivales@users.noreply.github.com>
2021-05-02 16:30:35 +0300

    Update 05_task_list.py

--- Module3/practice/list_and_tuple/05_task_list.py
+++ Module3/practice/list_and_tuple/05_task_list.py
@@ -5,7 +5,20 @@
 # Исходные данные:
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
-# TODO: your code here
+k = 1
+max_length = 0
+for fruit in fruits:
+    if len(fruit) > max_length:
+        max_length = len(fruit)
+for fruit in fruits:
+    if len(fruit) < max_length:
+        i = 0
+        dif_length=max_length-len(fruit)
+        while i < dif_length:
+            fruit = " " + fruit
+            i += 1
+    print(str(k) + '.', fruit)
+    k += 1
 
 # Пример вывода:
 # 1. яблоко
