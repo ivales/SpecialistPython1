8193e5c3741cde58cc10245b52b13ba814e14598
ivales <83123684+ivales@users.noreply.github.com>
2021-05-02 15:01:15 +0300

    Update add01_task_string.py

--- Module3/practice/string/add01_task_string.py
+++ Module3/practice/string/add01_task_string.py
@@ -6,5 +6,11 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat
        "Donec tincidunt sed lorem et feugiat. Nullam elementum"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 # Примечание: обратите внимание на перенос длинной строки на новую строку
-
-# TODO: your code here
+text_check=text.replace(',','').replace('.','').replace('!','').replace('?','').replace('-','')
+words = text_check.split(' ')
+counter=i=0
+while i<len(words):
+    if len(words[i])>7:
+        counter+=1
+    i+=1
+print(counter)
