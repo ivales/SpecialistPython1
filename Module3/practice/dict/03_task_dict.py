cc4d3ef7778ad4810549bb2b8c71c9ad2af0ee1c
ivales <83123684+ivales@users.noreply.github.com>
2021-05-03 14:15:24 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -28,21 +28,46 @@ staff = [
 ]
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
-
+max_salary=0
+for st in staff:
+    if st['salary']>max_salary:
+        max_salary=st['salary']
+        top_employee=st['name']+' '+st['surname']
+print(top_employee)
 # TODO: your code here
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
-
+min_salary=max_salary
+for st in staff:
+    if st['salary']<min_salary:
+        min_salary=st['salary']
+        bottom_employee=st['name']+' '+st['surname']
+print(bottom_employee)
 # TODO: your code here
 
 print("Среднеарифметическую зарплату всех сотрудников")
-
+aver_salary=0
+for st in staff:
+    aver_salary+=st['salary']
+aver_salary/=len(staff)
+print(aver_salary)
 # TODO: your code here
 
 print("Количество однофамильцев в организации")
-
+counter=0
+surnames=[]
+for st in staff:
+    surnames.append(st['surname'])
+print(surnames)
+for st in staff:
+    counter=+surnames.count(st['surname'])
+print(counter)
 # TODO: your code here
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
-
+staff_sorted = sorted(staff, key=lambda k: k['salary'])
+st_list=[]
+for st in staff_sorted:
+    st_list.append(str(st['name']+' '+st['surname']))
+print(st_list)
 # TODO: your code here
