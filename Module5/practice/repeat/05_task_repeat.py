338938e1ad4634eee1ef942a209a8eb65a20d468
ivales <83123684+ivales@users.noreply.github.com>
2021-05-03 16:40:26 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,7 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
-
+def last_page(num_items, items_on_page):
+    if num_items%items_on_page==0:
+        return 0
+    else:
+        return num_items%items_on_page
