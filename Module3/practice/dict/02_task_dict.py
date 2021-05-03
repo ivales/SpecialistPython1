ac74b1006c758a8c1747c55a1a65d4cf43000f30
ivales <83123684+ivales@users.noreply.github.com>
2021-05-03 11:54:31 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -3,7 +3,10 @@
 
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
-
+my_dict={}
+for i in range(len(keys)):
+    my_dict[keys[i]]=values[i]
+print(my_dict)
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
