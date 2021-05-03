b9274d4c67bbd66afb6cbf50db87d37929903458
ivales <83123684+ivales@users.noreply.github.com>
2021-05-03 15:30:20 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,9 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def time(secs):
+    hh=str(secs//3600)
+    mm=str((secs-int(hh)*3600)//60)
+    ss=str(secs-int(hh)*3600-int(mm)*60)
+    return hh.rjust(2, '0')+":"+mm.rjust(2, '0')+":"+ss.rjust(2, '0')
+print(time(29143))
