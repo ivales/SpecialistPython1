d564272c6089531ec195116165a7f9019a63433e
ivales <83123684+ivales@users.noreply.github.com>
2021-05-15 01:07:08 +0300

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -6,3 +6,23 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+items_dict = {}
+with open('data/items_sold.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        item_list = line.split()
+        for item in item_list:
+            pair = item.split(':')
+            if items_dict.get(pair[0]) is not None:
+                items_dict[pair[0]] += float(pair[1])
+            else:
+                items_dict[pair[0]] = float(pair[1])
+print(items_dict)
+print(sum(items_dict.values()))
+max_item = list(items_dict.values())[0]
+max_item_type=list(items_dict.keys())[0]
+for key,value in items_dict.items():
+    print(f'{key} : {value:.2f}')
+    if value > max_item:
+        max_item=value
+        max_item_type=key
+print(max_item_type)
